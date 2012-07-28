f6ed6e1341b1f3ec9f406d8dc93bdbe2c3da9e92
Hervé Weltzer <herve@Kashyyyk.local>
2012-07-28 23:23:26 +0200

    Autre test...

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -4,7 +4,7 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('hello, world! How are you?');
+		$this->response->body('hello, world! How are you? Good!');
 	}
 
 } // End Welcome
