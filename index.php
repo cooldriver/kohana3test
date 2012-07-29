7e136f6c3225b4463cf94bfd90c24338c82d0905
cooldriver <weltzer@gmail.com>
2012-07-29 13:31:37 +0200

    Test sans utiliser git add

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // Test très long
+// Lorem ipsum...
 /**
  * The directory in which your application specific resources are located.
  * The application directory must contain the bootstrap.php file.
